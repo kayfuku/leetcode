dfccf18d9d47cdf6c48c27309bc89010a2c3215c
Kei Fukutani <kifinewera@gmail.com>
2020-01-02 16:56:50 -0800

    updated.

---
+++ HashTable/ContainsDuplicate.java
@@ -0,0 +1,69 @@
+// 
+// Author: 
+// Date  : December 31, 2019
+
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+	// fields and classes here. 
+	//private int count;
+
+	public ContainsDuplicate() {
+		// Initialization here. 
+		//this.count = 0;
+
+	}
+
+
+	// Author: 
+	// Date  : December 31, 2019
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> set = new HashSet<>(nums.length);
+		for (int x : nums) {
+			if (set.contains(x)) {
+				return true;
+			}
+			set.add(x);
+		}
+		
+		return false;
+	}
+
+
+
+
+
+
+
+	// For testing. 
+	public static void main(String[] args) {
+		ContainsDuplicate solution = new ContainsDuplicate();
+
+		// Test arguments. 
+		// int num = 24;
+		// int target = 2;
+		// solution.getInt(num, target);
+
+
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ HashTable/HappyNumber.java
@@ -0,0 +1,76 @@
+// 
+// Author: 
+// Date  : December 31, 2019
+
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+	// fields and classes here. 
+	//private int count;
+
+	public HappyNumber() {
+		// Initialization here. 
+		//this.count = 0;
+		
+	}
+
+	
+	// Author: 
+	// Date  : January 2, 2020
+	private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+
+
+	
+	
+	
+
+	// For testing. 
+	public static void main(String[] args) {
+		HappyNumber solution = new HappyNumber();
+
+		// Test arguments. 
+		// int num = 24;
+		// int target = 2;
+		// solution.getInt(num, target);
+
+
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
