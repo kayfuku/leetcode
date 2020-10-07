3879c50bc497625212099eed4e0017de010f7751
Kei Fukutani <kifinewera@gmail.com>
2020-10-06 23:14:14 -0700

    updated.

--- DFS_BFS/NumberOfIslands.java
+++ DFS_BFS/NumberOfIslands.java
@@ -4,33 +4,35 @@
 
 package leetcode;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class NumberOfIslands {
-	// fields here. 
-	//	private int count;
+	// fields here.
+	// private int count;
 
 	public NumberOfIslands() {
-		// Initialization here. 
-		//		this.count = 0;
+		// Initialization here.
+		// this.count = 0;
 	}
 
-	// 1. DFS. The best solution is the BFS at the bottom. 
-	// O(MN) time, where M is the num of rows, and N is num of columns, 
-	// because we visit every square at most twice. 
-	// O(MN) space, in the case that the grid map is filled with lands 
-	// where DFS goes by M x N deep. 
+	// 1. DFS. The best solution is the BFS at the bottom.
+	// O(MN) time, where M is the num of rows, and N is num of columns,
+	// because we visit every square at most twice.
+	// O(MN) space, in the case that the grid map is filled with lands
+	// where DFS goes by M x N deep.
 	public int numIslands(char[][] grid) {
-		// Epithet code. 
+		// Epithet code.
 		if (grid == null || grid.length == 0) {
 			return 0;
 		}
 		int nr = grid.length;
 		int nc = grid[0].length;
 
-		// Iterate through the grid, and if we find a '1', then we start DFS. 
-		// And count the number of triggers of DFS. 
+		// Iterate through the grid, and if we find a '1', then we start DFS.
+		// And count the number of triggers of DFS.
 		int numIslands = 0;
 		for (int r = 0; r < nr; r++) {
 			for (int c = 0; c < nc; c++) {
@@ -43,35 +45,34 @@ public class NumberOfIslands {
 
 		return numIslands;
 	}
+
 	private void dfs(char[][] grid, int r, int c) {
-		if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || 
-				grid[r][c] == '0') {
+		if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == '0') {
 			return;
 		}
 
-		// Mark as visited.
+		// Mark as visited using '0'.
 		grid[r][c] = '0';
 
-		dfs(grid, r - 1, c);
-		dfs(grid, r + 1, c);
-		dfs(grid, r, c - 1);
-		dfs(grid, r, c + 1);
+		dfs(grid, r - 1, c); // up
+		dfs(grid, r + 1, c); // down
+		dfs(grid, r, c - 1); // left
+		dfs(grid, r, c + 1); // right
 	}
 
-
-	// 2. BFS. The bottom one is the best. 
-	// O(MN) time, because we visit every square at most twice. 
-	// O(M + N) space, the size of the queue can grow up at most 
-	// the perimeter of the grid. 
+	// 2. BFS. The bottom one is the best.
+	// O(MN) time, because we visit every square at most twice.
+	// O(M + N) space, the size of the queue can grow up at most
+	// the perimeter of the grid.
 	public int numIslands2(char[][] grid) {
-		// Epithet code. 
+		// Epithet code.
 		if (grid == null || grid.length == 0) {
 			return 0;
 		}
 		int nr = grid.length;
 		int nc = grid[0].length;
 
-		// Iterate through the grid, and if we find a '1', then we start BFS. 
+		// Iterate through the grid, and if we find a '1', then we start BFS.
 		// And count the number of triggers of BFS.
 		int numIslands = 0;
 		for (int r = 0; r < nr; r++) {
@@ -85,41 +86,42 @@ public class NumberOfIslands {
 
 		return numIslands;
 	}
+
 	private void bfs(char[][] grid, int r, int c) {
 		int nr = grid.length;
 		int nc = grid[0].length;
 
 		Queue<Integer> queue = new LinkedList<>();
-		// Technique not to use double queues. 
-		// 'r * nc + c' contains info of both row and column. 
-		// Note that it starts from 0; 
-		// => How about int[]? => Yes. See the bottom. 
+		// Technique not to use double queues.
+		// 'r * nc + c' contains info of both row and column.
+		// Note that it starts from 0;
+		// => How about int[]? => Yes. See the bottom.
 		queue.add(r * nc + c);
-		// Mark as visited. 
+		// Mark as visited.
 		grid[r][c] = '0';
 		while (!queue.isEmpty()) {
 			int rc = queue.poll();
 			int row = rc / nc;
 			int col = rc % nc;
 
-			// Neighbors. 
-			// Up. 
+			// Neighbors.
+			// Up.
 			if (row - 1 >= 0 && grid[row - 1][col] == '1') {
 				queue.add((row - 1) * nc + col);
-				// Mark as visited. 
+				// Mark as visited.
 				grid[row - 1][col] = '0';
 			}
-			// Down. 
+			// Down.
 			if (row + 1 < nr && grid[row + 1][col] == '1') {
 				queue.add((row + 1) * nc + col);
 				grid[row + 1][col] = '0';
 			}
-			// Left. 
+			// Left.
 			if (col - 1 >= 0 && grid[row][col - 1] == '1') {
 				queue.add(row * nc + (col - 1));
 				grid[row][col - 1] = '0';
 			}
-			// Right. 
+			// Right.
 			if (col + 1 < nc && grid[row][col + 1] == '1') {
 				queue.add(row * nc + (col + 1));
 				grid[row][col + 1] = '0';
@@ -127,27 +129,28 @@ public class NumberOfIslands {
 		}
 	}
 
-
-	// 3. Union Find. 
-	// Author: imsure + kei
-	// Date  : July 31, 2019
+	// 3. Union Find.
+	// Author: imsure + kei (AC)
+	// Date : July 31, 2019
 	class UnionFind {
-		int count; // # of connected components <= Is it ok? 
+		int count; // # of islands
 		int[] parent;
 		int[] rank;
 
+		// Initialization
 		public UnionFind(char[][] grid) { // for problem 200
 			count = 0;
 			int m = grid.length;
 			int n = grid[0].length;
 			parent = new int[m * n];
 			rank = new int[m * n];
-			for (int i = 0; i < m; ++i) {
-				for (int j = 0; j < n; ++j) {
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
 					if (grid[i][j] == '1') {
-						// Initialization. Every node has its own node as a parent node. 
-						// Store the coordinate [i, j] itself as the parent coordinate. 
+						// Initialization. Every node has its own node as a parent node.
+						// Store the coordinate [i, j] itself as the parent coordinate.
 						parent[i * n + j] = i * n + j;
+						// Count the number of '1'
 						count++;
 					}
 					rank[i * n + j] = 0;
@@ -156,11 +159,11 @@ public class NumberOfIslands {
 		}
 
 		// ex)
-		// 2 - 0 - 3 
-		//     |     
-		//     1 - 4  
-		// i: 0 1 2 3 4     find(4) i: 0 1 2 3 4
-		// p: 2 0 2 0 1  =>         p: 2 2 2 0 2
+		// 2 - 0 - 3
+		// |
+		// 1 - 4
+		// i: 0 1 2 3 4 find(4) i: 0 1 2 3 4
+		// p: 2 0 2 0 1 ======> p: 2 2 2 0 2
 		public int find(int i) { // path compression <= find root + path compression?
 			if (parent[i] != i) {
 				parent[i] = find(parent[i]);
@@ -168,21 +171,24 @@ public class NumberOfIslands {
 			return parent[i];
 		}
 
-		// Connect the components. 
+		// Connect the components.
 		public void union(int x, int y) { // union with rank
 			int rootx = find(x);
 			int rooty = find(y);
 			if (rootx != rooty) {
 				if (rank[rootx] > rank[rooty]) {
+					// Add lower tree y to higher tree x.
+					// The parent of rooty is rootx.
 					parent[rooty] = rootx;
-					rank[rootx]++; // I added. ok?
 				} else if (rank[rootx] < rank[rooty]) {
+					// Add lower tree x to higher tree y.
 					parent[rootx] = rooty;
-					rank[rooty]++; // I added. ok?
 				} else {
-					parent[rooty] = rootx; 
+					parent[rooty] = rootx;
 					rank[rootx]++;
 				}
+				// Decrement the count because we just connected '1' and '1'.
+				// 'count' is eventually the number of islands.
 				count--;
 			}
 		}
@@ -199,27 +205,27 @@ public class NumberOfIslands {
 
 		int nr = grid.length;
 		int nc = grid[0].length;
-		
+
 		UnionFind uf = new UnionFind(grid);
 		for (int r = 0; r < nr; r++) {
 			for (int c = 0; c < nc; c++) {
 				if (grid[r][c] == '1') {
-					// Mark as visited. 
+					// Mark as visited.
 					grid[r][c] = '0';
-					
-					// Up. 
+
+					// Up.
 					if (r - 1 >= 0 && grid[r - 1][c] == '1') {
 						uf.union(r * nc + c, (r - 1) * nc + c);
 					}
-					// Down. 
+					// Down.
 					if (r + 1 < nr && grid[r + 1][c] == '1') {
 						uf.union(r * nc + c, (r + 1) * nc + c);
 					}
-					// Left. 
+					// Left.
 					if (c - 1 >= 0 && grid[r][c - 1] == '1') {
 						uf.union(r * nc + c, r * nc + c - 1);
 					}
-					// Right. 
+					// Right.
 					if (c + 1 < nc && grid[r][c + 1] == '1') {
 						uf.union(r * nc + c, r * nc + c + 1);
 					}
@@ -229,114 +235,74 @@ public class NumberOfIslands {
 
 		return uf.getCount();
 	}
-	
-	
-	// Review. This is the best! 
+
+	// Review. This is the best!
 	// Author: kei (AC)
-	// Date  : January 6, 2020
+	// Date : January 6, 2020
 	public int numIslandsR(char[][] grid) {
-		// Epithet code. 
+		// Epithet code.
 		if (grid == null || grid.length == 0) {
 			return 0;
 		}
-		int nr = grid.length;
-		int nc = grid[0].length;
+		int R = grid.length;
+		int C = grid[0].length;
 
-		// Iterate through the grid, and if we find a '1', then we start BFS. 
+		// Iterate through the grid, and if we find a '1', then we start BFS.
 		// And count the number of triggers of BFS.
 		int numIslands = 0;
-		for (int r = 0; r < nr; r++) {
-			for (int c = 0; c < nc; c++) {
+		for (int r = 0; r < R; r++) {
+			for (int c = 0; c < C; c++) {
 				if (grid[r][c] == '1') {
 					numIslands++;
-					bfsR(grid, r, c);
+					bfsR2(grid, r, c);
 				}
 			}
 		}
 
 		return numIslands;
 	}
-	private void bfsR(char[][] grid, int r, int c) {
-		int nr = grid.length;
-		int nc = grid[0].length;
-	
+
+	private static final char VISITED = '0';
+	private static final int[] ROW_DIR = new int[] { -1, 1, 0, 0 };
+	private static final int[] COL_DIR = new int[] { 0, 0, -1, 1 };
+
+	private void bfsR2(char[][] grid, int r, int c) {
+		int R = grid.length;
+		int C = grid[0].length;
+
 		Queue<int[]> queue = new LinkedList<>();
-		queue.add(new int[]{ r, c });
-		// Mark as visited. 
-		grid[r][c] = '0';
-		
+		queue.add(new int[] { r, c });
+		// Mark as visited.
+		grid[r][c] = VISITED;
+
 		while (!queue.isEmpty()) {
 			int[] square = queue.poll();
 			int row = square[0];
 			int col = square[1];
 
-			// Neighbors. 
-			// Up. 
-			if (row - 1 >= 0 && grid[row - 1][col] == '1') {
-				queue.add(new int[]{ row - 1, col });
-				// Mark as visited. 
-				grid[row - 1][col] = '0';
+			// Neighbors.
+			for (int i = 0; i < 4; i++) {
+				int nr = row + ROW_DIR[i];
+				int nc = col + COL_DIR[i];
+				if (nr < 0 || nc < 0 || nr >= R || nc >= C || grid[nr][nc] == VISITED) {
+					continue;
+				}
+				queue.add(new int[] { nr, nc });
+				grid[nr][nc] = VISITED;
 			}
-			// Down. 
-			if (row + 1 < nr && grid[row + 1][col] == '1') {
-				queue.add(new int[]{ row + 1, col });
-				grid[row + 1][col] = '0';
-			}
-			// Left. 
-			if (col - 1 >= 0 && grid[row][col - 1] == '1') {
-				queue.add(new int[]{ row, col - 1 });
-				grid[row][col - 1] = '0';
-			}
-			// Right. 
-			if (col + 1 < nc && grid[row][col + 1] == '1') {
-				queue.add(new int[]{ row, col + 1 });
-				grid[row][col + 1] = '0';
-			}
+
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
-
-	// For testing. 
+	// For testing.
 	public static void main(String[] args) {
 		NumberOfIslands solution = new NumberOfIslands();
 
-		// Test arguments. 
+		// Test arguments.
 		// int num = 24;
 		// int target = 2;
 		// solution.getInt(num, target);
 
-
-
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
