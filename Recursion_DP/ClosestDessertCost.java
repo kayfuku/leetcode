82e53b1e01a1e21eef7b7293091a5eaa86a7d116
Kei Fukutani <kifinewera@gmail.com>
2021-03-06 18:08:57 -0800

    update

--- Recursion_DP/ClosestDessertCost.java
+++ Recursion_DP/ClosestDessertCost.java
@@ -12,9 +12,39 @@ public class ClosestDessertCost {
 
   }
 
+  // Author: zerotrac2 + kei
+  // Date : March 6, 2021
+  private int ans = -1;
+
+  public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+    int n = baseCosts.length;
+    for (int i = 0; i < n; i++) {
+      dfs(baseCosts[i], toppingCosts, 0, target);
+    }
+    return ans;
+  }
+
+  private void dfs(int cost, int[] toppingCosts, int j, int target) {
+    if (j == toppingCosts.length) {
+      // Now we considered all the toppings.
+      if (ans == -1 || //
+          Math.abs(cost - target) < Math.abs(ans - target) || //
+          (Math.abs(cost - target) == Math.abs(ans - target) && cost < ans)) {
+        ans = cost;
+      }
+      return;
+    }
+    // no topping
+    dfs(cost, toppingCosts, j + 1, target);
+    // one topping of this kind
+    dfs(cost + toppingCosts[j], toppingCosts, j + 1, target);
+    // two toppings of this kind
+    dfs(cost + toppingCosts[j] * 2, toppingCosts, j + 1, target);
+  }
+
   // Author: uwi + kei
   // Date : February 28, 2021
-  public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+  public int closestCost2(int[] baseCosts, int[] toppingCosts, int target) {
     // Looking at the restrictions,
     //
     // m == toppingCosts.length
@@ -22,26 +52,28 @@ public class ClosestDessertCost {
     // 1 <= baseCosts[i], toppingCosts[i] <= 10^4
     // 1 <= target <= 10^4
     //
-    // TODO: The maximum toppings cost is ... ??
+    // The maximum toppings cost we need to think about is 20000 because the target
+    // is at most 10000.
     // i: sum of toppings cost
     // dp[i]: True if that case exists.
-    boolean[] dp = new boolean[30000];
+    boolean[] dp = new boolean[20000];
     dp[0] = true;
     for (int t : toppingCosts) {
-      for (int i = 29999; i >= 0; i--) {
+      for (int i = 19999; i >= 0; i--) {
         if (dp[i]) {
-          if (i + t < 30000) {
+          if (i + t < 20000) {
             dp[i + t] = true;
           }
-          if (i + 2 * t < 30000) {
+          if (i + 2 * t < 20000) {
             dp[i + 2 * t] = true;
           }
         }
       }
     }
-    int best = 99999999;
+
+    int best = Integer.MAX_VALUE;
     for (int b : baseCosts) {
-      for (int i = 0; i < 30000; i++) {
+      for (int i = 0; i < 20000; i++) {
         if (dp[i]) {
           int v = b + i;
           if (Math.abs(v - target) < Math.abs(best - target)
@@ -51,13 +83,14 @@ public class ClosestDessertCost {
         }
       }
     }
+
     return best;
   }
 
-  //
+  // Well, creating a set is good, but it could be more efficient.
   // Author: kei (AC)
   // Date : February 28, 2021
-  public int closestCost2(int[] baseCosts, int[] toppingCosts, int target) {
+  public int closestCost3(int[] baseCosts, int[] toppingCosts, int target) {
     Set<Integer> toppingSet = new HashSet<>();
     for (int i = 0; i < toppingCosts.length; i++) {
       getSet(i, toppingCosts, 0, toppingSet);
@@ -82,9 +115,6 @@ public class ClosestDessertCost {
 
     return ans;
   }
-
-  // K: sum,
-  Map<Integer, Integer> memo = new HashMap<>();
 
   private void getSet(int i, int[] toppingCosts, int sum, Set<Integer> toppingSet) {
     if (i == toppingCosts.length) {
