6631e2c0296f4343a03948cdce2b0dc512d5d44b
Kei Fukutani <kifinewera@gmail.com>
2022-05-22 12:15:01 +0900

    update

--- HashTable/ContainsDuplicate.java
+++ HashTable/ContainsDuplicate.java
@@ -8,18 +8,17 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ContainsDuplicate {
-	// fields and classes here. 
-	//private int count;
+	// fields and classes here.
+	// private int count;
 
 	public ContainsDuplicate() {
-		// Initialization here. 
-		//this.count = 0;
+		// Initialization here.
+		// this.count = 0;
 
 	}
 
-
-	// Author: 
-	// Date  : December 31, 2019
+	// Author: leetcode + kei
+	// Date : December 31, 2019
 	public boolean containsDuplicate(int[] nums) {
 		Set<Integer> set = new HashSet<>(nums.length);
 		for (int x : nums) {
@@ -28,42 +27,21 @@ public class ContainsDuplicate {
 			}
 			set.add(x);
 		}
-		
+
 		return false;
 	}
 
+	// For testing.
+	@SuppressWarnings("unused")
 
-
-
-
-
-
-	// For testing. 
 	public static void main(String[] args) {
 		ContainsDuplicate solution = new ContainsDuplicate();
 
-		// Test arguments. 
+		// Test arguments.
 		// int num = 24;
 		// int target = 2;
 		// solution.getInt(num, target);
 
-
-
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
