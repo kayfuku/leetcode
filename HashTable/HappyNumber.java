e5696fb78f763028ba34cca809dd53e1b1949b56
Kei Fukutani <kifinewera@gmail.com>
2022-05-24 10:21:50 +0900

    update

--- HashTable/HappyNumber.java
+++ HashTable/HappyNumber.java
@@ -8,69 +8,46 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class HappyNumber {
-	// fields and classes here. 
-	//private int count;
+	// fields and classes here.
+	// private int count;
 
 	public HappyNumber() {
-		// Initialization here. 
-		//this.count = 0;
-		
+		// Initialization here.
+		// this.count = 0;
+
 	}
 
-	
-	// Author: 
-	// Date  : January 2, 2020
+	// Author:
+	// Date : January 2, 2020
 	private int getNext(int n) {
-        int totalSum = 0;
-        while (n > 0) {
-            int d = n % 10;
-            n = n / 10;
-            totalSum += d * d;
-        }
-        return totalSum;
-    }
+		int totalSum = 0;
+		while (n > 0) {
+			int d = n % 10;
+			n = n / 10;
+			totalSum += d * d;
+		}
+		return totalSum;
+	}
 
-    public boolean isHappy(int n) {
-        Set<Integer> seen = new HashSet<>();
-        while (n != 1 && !seen.contains(n)) {
-            seen.add(n);
-            n = getNext(n);
-        }
-        return n == 1;
-    }
+	public boolean isHappy(int n) {
+		Set<Integer> seen = new HashSet<>();
+		while (n != 1 && !seen.contains(n)) {
+			seen.add(n);
+			n = getNext(n);
+		}
+		return n == 1;
+	}
 
-
-
-	
-	
-	
-
-	// For testing. 
+	// For testing.
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		HappyNumber solution = new HappyNumber();
 
-		// Test arguments. 
+		// Test arguments.
 		// int num = 24;
 		// int target = 2;
 		// solution.getInt(num, target);
 
-
-
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- HashTable/IsomorphicStrings.java
+++ HashTable/IsomorphicStrings.java
@@ -5,75 +5,52 @@
 package leetcode;
 
 public class IsomorphicStrings {
-	// fields and classes here. 
-	//private int count;
+	// fields and classes here.
+	// private int count;
 
 	public IsomorphicStrings() {
-		// Initialization here. 
-		//this.count = 0;
+		// Initialization here.
+		// this.count = 0;
 
 	}
 
-
-	// Use a map to keep track of the information about the same character. 
-	// Use index to hold order information. 
+	// Use a map to keep track of the information about the same character.
+	// Use index to hold order information.
 	// Author: grandyang + kei
-	// Date  : January 2, 2020
+	// Date : January 2, 2020
 	public boolean isIsomorphic(String s, String t) {
 		int[] m1 = new int[256];
 		int[] m2 = new int[256];
 		int n = s.length();
-		
+
 		for (int i = 0; i < n; i++) {
 			char c1 = s.charAt(i);
 			char c2 = t.charAt(i);
-			// If the previous index of the same character is the same, 
-			// then structure is the same. 
-			// Same character should have the same index. 
+			// If the previous index of the same character is the same,
+			// then structure is the same.
+			// Same character should have the same index.
 			if (m1[c1] != m2[c2]) {
 				return false;
 			}
-			// Put the index except 0 to hold order information. 
+			// Put the index except 0 to hold order information.
 			// Counting cannot hold the order information.
 			m1[c1] = i + 1;
 			m2[c2] = i + 1;
 		}
-		
+
 		return true;
 	}
 
-
-
-
-
-
-
-	// For testing. 
+	// For testing.
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		IsomorphicStrings solution = new IsomorphicStrings();
 
-		// Test arguments. 
+		// Test arguments.
 		// int num = 24;
 		// int target = 2;
 		// solution.getInt(num, target);
 
-
-
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
