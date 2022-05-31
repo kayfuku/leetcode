51865d284a57c51db6c91e02331094520b75aaa0
Kei Fukutani <kifinewera@gmail.com>
2022-05-31 10:31:05 +0900

    update

--- HashTable/FindDuplicateSubtrees.java
+++ HashTable/FindDuplicateSubtrees.java
@@ -10,19 +10,18 @@ import java.util.List;
 import java.util.Map;
 
 public class FindDuplicateSubtrees {
-	// fields and classes here. 
-	//private int count;
+	// fields and classes here.
+	// private int count;
 
 	public FindDuplicateSubtrees() {
-		// Initialization here. 
-		//this.count = 0;
+		// Initialization here.
+		// this.count = 0;
 
 	}
 
-
-	// O(N) time, O(N) space. 
+	// O(N) time, O(N) space.
 	// Author: awice + StefanPochmann + kei
-	// Date  : January 3, 2020
+	// Date : January 3, 2020
 	int t;
 	Map<String, Integer> trees;
 	Map<Integer, Integer> count;
@@ -41,45 +40,40 @@ public class FindDuplicateSubtrees {
 		if (node == null) {
 			return 0;
 		}
-		
-		// Make an id for this tree and count the same tree. 
-		// Serialize the tree. 
+
+		// Make an id for this tree and count the same tree.
+		// Serialize the tree.
 		String serial = node.val + "," + lookup(node.left) + "," + lookup(node.right);
-		// computeIfAbsent(key, function) use the function if the key is not in the map. 
-		// lambda function returns t++. 
+		// computeIfAbsent(key, function) use the function if the key is not in the map.
+		// lambda function returns t++.
 		int uid = trees.computeIfAbsent(serial, x -> t++);
 		count.put(uid, count.getOrDefault(uid, 0) + 1);
-		
+
 		if (count.get(uid) == 2) {
 			ans.add(node);
 		}
-		
+
 		return uid;
 	}
 
-
-
-
-
-
-
-	// For testing. 
+	// For testing.
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		FindDuplicateSubtrees solution = new FindDuplicateSubtrees();
 
-		// Test arguments. 
+		// Test arguments.
 		// int num = 24;
 		// int target = 2;
 		// solution.getInt(num, target);
-		
+
 		// Binary Tree
-		//       1
-		//      / \
-		//     2   3 
-		//    /   / \
-		//   4   2   4
-		//      /
-		//     4
+		// 1
+		// / \
+		// 2 3
+		// / / \
+		// 4 2 4
+		// /
+		// 4
 		TreeNode n1 = new TreeNode(1);
 		TreeNode n2 = new TreeNode(2);
 		TreeNode n3 = new TreeNode(3);
@@ -94,25 +88,7 @@ public class FindDuplicateSubtrees {
 		n3.right = n6;
 		n5.left = n7;
 		System.out.println(solution.findDuplicateSubtrees(n1));
-		
-
-
 
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
