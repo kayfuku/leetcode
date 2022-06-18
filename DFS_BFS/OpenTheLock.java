f41bd1bf50ba9dd937d800d3ba7e6bd06e2b918a
Kei Fukutani <kifinewera@gmail.com>
2022-06-18 10:48:51 +0900

    update

--- DFS_BFS/OpenTheLock.java
+++ DFS_BFS/OpenTheLock.java
@@ -11,19 +11,18 @@ import java.util.Queue;
 import java.util.Set;
 
 public class OpenTheLock {
-	// fields and classes here. 
-	//private int count;
+	// fields and classes here.
+	// private int count;
 
 	public OpenTheLock() {
-		// Initialization here. 
-		//this.count = 0;
+		// Initialization here.
+		// this.count = 0;
 
 	}
 
-
 	// BFS
 	// Author: yaoyimingg + kei
-	// Date  : January 7, 2020
+	// Date : January 7, 2020
 	public int openLock(String[] deadends, String target) {
 		Queue<String> q = new LinkedList<>();
 		Set<String> deads = new HashSet<>(Arrays.asList(deadends));
@@ -36,12 +35,12 @@ public class OpenTheLock {
 		q.offer("0000");
 		visited.add("0000");
 
-		// BFS with level count. 
+		// BFS with level count.
 		int numTurns = 0;
 		while (!q.isEmpty()) {
-			
+
 			int size = q.size();
-			
+
 			// For each level
 			while (size > 0) {
 				String s = q.poll();
@@ -55,9 +54,9 @@ public class OpenTheLock {
 				for (int i = 0; i < 4; i++) {
 					char c = sb.charAt(i);
 
-					// Dial up. c - '0' converts char to int. 
+					// Dial up. c - '0' converts char to int.
 					String s1 = sb.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + sb.substring(i + 1);
-					// Dial down. 
+					// Dial down.
 					String s2 = sb.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + sb.substring(i + 1);
 
 					if (!visited.contains(s1) && !deads.contains(s1)) {
@@ -69,61 +68,60 @@ public class OpenTheLock {
 						visited.add(s2);
 					}
 				}
-				
+
 				size--;
 			}
-			
+
 			numTurns++;
 		}
 
 		return -1;
 	}
 
-
 	// 2-end BFS (Bidirectional BFS?)
-	// yaoyimingg talks about the improvement, which makes it three times faster. 
+	// yaoyimingg talks about the improvement, which makes it three times faster.
 	// https://leetcode.com/problems/open-the-lock/discuss/110237/Regular-java-BFS-solution-and-2-end-BFS-solution-with-improvement
 	// Author: yaoyimingg + kei
-	// Date  : January 7, 2020
+	// Date : January 7, 2020
 	class Solution {
 		public int openLock(String[] deadends, String target) {
-			// Note that these are sets, not queues. 
+			// Note that these are sets, not queues.
 			Set<String> begin = new HashSet<>();
 			Set<String> end = new HashSet<>();
-			// deads plays a visited role as well. 
+			// deads plays a visited role as well.
 			Set<String> deads = new HashSet<>(Arrays.asList(deadends));
-			
+
 			if (deads.contains("0000")) {
 				return -1;
 			}
 			begin.add("0000");
 			end.add(target);
-			
+
 			int level = 0;
 			Set<String> temp;
-			while(!begin.isEmpty() && !end.isEmpty()) {
+			while (!begin.isEmpty() && !end.isEmpty()) {
 				if (begin.size() > end.size()) {
 					temp = begin;
 					begin = end;
 					end = temp;
 				}
-				// Assert that begin set is smaller. 
-				
-				// Since we use sets, the next layer should be a different set. 
-				// Otherwise they would be mixed. 
+				// Assert that begin set is smaller.
+
+				// Since we use sets, the next layer should be a different set.
+				// Otherwise they would be mixed.
 				temp = new HashSet<>();
 				for (String s : begin) {
-					// Intersect. 
+					// Intersect.
 					if (end.contains(s)) {
 						return level;
 					}
-					
+
 					deads.add(s);
 					for (int i = 0; i < 4; i++) {
 						char c = s.charAt(i);
 						// Dial up.
 						String s1 = s.substring(0, i) + (c == '9' ? 0 : c - '0' + 1) + s.substring(i + 1);
-						// Dial down. 
+						// Dial down.
 						String s2 = s.substring(0, i) + (c == '0' ? 9 : c - '0' - 1) + s.substring(i + 1);
 						if (!deads.contains(s1)) {
 							temp.add(s1);
@@ -133,47 +131,25 @@ public class OpenTheLock {
 						}
 					}
 				}
-				
+
 				level++;
 				begin = temp;
 			}
-			
+
 			return -1;
 		}
 	}
 
-
-
-
-
-
-
-	// For testing. 
+	// For testing.
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		OpenTheLock solution = new OpenTheLock();
 
-		// Test arguments. 
+		// Test arguments.
 		// int num = 24;
 		// int target = 2;
 		// solution.getInt(num, target);
 
-
-
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ Recursion_DP/PerfectSquares.java
@@ -0,0 +1,36 @@
+// 
+// Author: 
+// Date  : January 7, 2020
+
+package leetcode;
+
+public class PerfectSquares {
+	// fields and classes here.
+	// private int count;
+
+	public PerfectSquares() {
+		// Initialization here.
+		// this.count = 0;
+
+	}
+
+	// Author:
+	// Date : January 7, 2020
+	public int numSquares(int n) {
+
+		return 0;
+	}
+
+	// For testing.
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		PerfectSquares solution = new PerfectSquares();
+
+		// Test arguments.
+		// int num = 24;
+		// int target = 2;
+		// solution.getInt(num, target);
+
+	}
+
+}
