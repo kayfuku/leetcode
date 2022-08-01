4d7d8ff5076430999c34646aeea0dde6f37d3529
Kei Fukutani <kifinewera@gmail.com>
2022-08-01 17:05:18 +0900

    update

--- Recursion_DP/SudokuSolver.java
+++ Recursion_DP/SudokuSolver.java
@@ -14,16 +14,15 @@ public class SudokuSolver {
 
   }
 
-
-  // Author: leetcode + kei 
-  // Date  : September 18, 2020
+  // Author: leetcode + kei
+  // Date : September 18, 2020
 
   // Box size
   int n = 3;
   // Row size
   int N = n * n;
 
-  // To check if each of the digits 1-9 must occur exactly once. 
+  // To check if each of the digits 1-9 must occur exactly once.
   int[][] rows = new int[N][N + 1];
   int[][] columns = new int[N][N + 1];
   int[][] boxes = new int[N][N + 1];
@@ -35,7 +34,7 @@ public class SudokuSolver {
   public void solveSudoku(char[][] board) {
     this.board = board;
 
-    // Init rows, columns and boxes. 
+    // Init rows, columns and boxes.
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
         char num = board[i][j];
@@ -45,7 +44,7 @@ public class SudokuSolver {
         }
       }
     }
-    
+
     backtrack(0, 0);
   }
 
@@ -56,8 +55,8 @@ public class SudokuSolver {
       return;
     }
 
-    // The cell is empty. 
-    // Iterate over all numbers from 1 to 9. 
+    // The cell is empty.
+    // Iterate over all numbers from 1 to 9.
     for (int d = 1; d < 10; d++) {
       if (couldPlace(d, row, col)) {
         placeNumber(d, row, col);
@@ -65,21 +64,21 @@ public class SudokuSolver {
         if (sudokuSolved) {
           break;
         }
-        // Recover.  
-        removeNumber(d, row, col); 
+        // Recover.
+        removeNumber(d, row, col);
       }
     }
   }
 
-  // Check if one could place a number d in (row, col) cell.   
+  // Check if one could place a number d in (row, col) cell.
   public boolean couldPlace(int d, int row, int col) {
     // Box index
     int idx = (row / n) * n + col / n;
-    // Check if there is no 'd' in the row, col, and box. 
+    // Check if there is no 'd' in the row, col, and box.
     return rows[row][d] + columns[col][d] + boxes[idx][d] == 0;
   }
 
-  // Place a number d in (row, col) cell. 
+  // Place a number d in (row, col) cell.
   public void placeNumber(int d, int row, int col) {
     // Box index
     int idx = (row / n) * n + col / n;
@@ -87,28 +86,28 @@ public class SudokuSolver {
     rows[row][d]++;
     columns[col][d]++;
     boxes[idx][d]++;
-    // Convert int number to corresponding char and put it in the board. 
+    // Convert int number to corresponding char and put it in the board.
     board[row][col] = (char) (d + '0');
   }
 
   // Call backtrack function recursively to continue to place numbers
-  // till we have a solution. 
-  public void moveToNextSquare(int row, int col) { 
+  // till we have a solution.
+  public void moveToNextSquare(int row, int col) {
     if ((col == N - 1) && (row == N - 1)) {
-      // We're in the last cell. 
-      // That means we have the solution. 
+      // We're in the last cell.
+      // That means we have the solution.
       sudokuSolved = true;
     } else if (col == N - 1) {
-      // We're in the end of the row. 
-      // Go to the next row and col 0.  
+      // We're in the end of the row.
+      // Go to the next row and col 0.
       backtrack(row + 1, 0);
     } else {
-      // Go to the next column. 
+      // Go to the next column.
       backtrack(row, col + 1);
     }
   }
 
-  // Remove a number which didn't lead to a solution. 
+  // Remove a number which didn't lead to a solution.
   public void removeNumber(int d, int row, int col) {
     int idx = (row / n) * n + col / n;
 
@@ -117,11 +116,6 @@ public class SudokuSolver {
     boxes[idx][d]--;
     board[row][col] = '.';
   }
-
-
-
-
-
 
   // For testing.
   @SuppressWarnings("unused")
@@ -133,13 +127,6 @@ public class SudokuSolver {
     // int target = 2;
     // solution.getInt(num, target);
 
-
-
   }
 
-
-
-
 }
-
-
