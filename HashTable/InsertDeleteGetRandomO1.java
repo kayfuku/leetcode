ceee049950fe05fd8a0def51e2a49e1ba8bae1fc
Kei Fukutani <kifinewera@gmail.com>
2022-08-22 10:17:09 +0900

    update

--- HashTable/InsertDeleteGetRandomO1.java
+++ HashTable/InsertDeleteGetRandomO1.java
@@ -20,16 +20,16 @@ public class InsertDeleteGetRandomO1 {
 
   }
 
-  // To get random data, we need an array to store data. 
-  // To remove data in O(1) time, we need to copy the last element  
-  // to the element to be deleted, then delete the last element. 
-  // Since we first need to know the index of the element to be deleted, 
+  // To get random data, we need an array to store data.
+  // To remove data in O(1) time, we need to copy the last element
+  // to the element to be deleted, then delete the last element.
+  // Since we first need to know the index of the element to be deleted,
   // we need a map to keep track of the indices of them.
-  // 
+  //
   // Author: leetcode + kei
   // Date : October 16, 2020
   class RandomizedSet {
-    
+
     List<Integer> list;
     // K: val, V: index
     Map<Integer, Integer> map;
@@ -37,27 +37,27 @@ public class InsertDeleteGetRandomO1 {
 
     /** Initialize your data structure here. */
     public RandomizedSet() {
-      list = new ArrayList();
-      map = new HashMap();
+      list = new ArrayList<Integer>();
+      map = new HashMap<Integer, Integer>();
     }
 
     /**
      * Inserts a value to the set. Returns true if the set did not already contain
      * the specified element.
-     * O(1) time. 
+     * O(1) time.
      */
     public boolean insert(int val) {
       if (map.containsKey(val)) {
         return false;
       }
-      
-      // Add it at the tail in the array.     
+
+      // Add it at the tail in the array.
       list.add(val);
-      // We need to keep track of indices of vals using map in order to delete 
-      // them in O(1) time. 
-      // We need to be careful about putting the index in the map because the 
-      // size changes once adding it. 
-      // list.size() - 1 indicates the last index of the array. 
+      // We need to keep track of indices of vals using map in order to delete
+      // them in O(1) time.
+      // We need to be careful about putting the index in the map because the
+      // size changes once adding it.
+      // list.size() - 1 indicates the last index of the array.
       map.put(val, list.size() - 1);
 
       return true;
@@ -72,16 +72,17 @@ public class InsertDeleteGetRandomO1 {
         return false;
       }
 
-      // Copy the last element to the val to be deleted in the list and then 
-      // delete the last element later.   
+      // Copy the last element to the val to be deleted in the list and then
+      // delete the last element later.
       // If you remove without moving the val to the last element, then shifting will
       // occur, which takes O(N) time.
       int lastElement = list.get(list.size() - 1);
-      // This is exactly why we need a map for this problem. When we know the element 
-      // to be deleted but don't know the index, we cannot copy over to it in O(1) time. 
+      // This is exactly why we need a map for this problem. When we know the element
+      // to be deleted but don't know the index, we cannot copy over to it in O(1)
+      // time.
       int idx = map.get(val);
       list.set(idx, lastElement);
-      // Update the index of the copied element. 
+      // Update the index of the copied element.
       map.put(lastElement, idx);
 
       // Delete the last element.
